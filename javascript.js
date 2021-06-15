505a4d1b05ec885e8abfae017c114e605a9659e4
Sebastian Drewes <sebas.drewes@gmail.com>
2021-06-15 11:37:18 -0300

    agregados comments y mejorado style

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
+// def DOM
 const img = document.querySelectorAll('.img');
 const leftArrow = document.querySelector('#leftArrow');
 const rightArrow = document.querySelector('#rightArrow');
+const spheresContainer = document.querySelector('.spheresContainer');
+// a primer imagen del node, actualiza display
 img[0].style.display = 'block';
 let currentImage = 0;
+// selecciona node esfera, y agrega class a esfera seleccionada
+// borra class de todas las esferas distintas
 const sphereColor = (i) => {
   const sphere = document.querySelectorAll('.sphere');
   sphere[i].classList.add('sphereClicked');
@@ -39,17 +44,21 @@ leftArrow.addEventListener('click', () => {
     sphereColor(currentImage);
   }
 });
-
-const spheresContainer = document.querySelector('.spheresContainer');
+// creacion dinamica de esferas por imagen
 for (let i = 0; i < img.length; i += 1) {
   const sphere = document.createElement('div');
+  // a cada esfera, se la linkea con el index de imagen correspondiente
+  // para poder actualizar display con esa imagen
+  // y actualiza currentImage para mantener funcionamiento arrows
   // eslint-disable-next-line no-loop-func
   sphere.addEventListener('click', () => {
     img[i].style.display = 'block';
     for (let j = 0; j < img.length; j += 1) {
+      // cambia display de todas las otras imagenes, menos la clickeada
       if (img[j] !== img[i]) {
         img[j].style.display = 'none';
         currentImage = i;
+        // funcion colorear esfera
         sphereColor(i);
       }
     }
@@ -57,8 +66,9 @@ for (let i = 0; i < img.length; i += 1) {
   sphere.classList.add('sphere');
   spheresContainer.appendChild(sphere);
 }
+// pinta primer esfera
 sphereColor(0);
-
+// mueve una imagen cada 5 seg
 setInterval(() => {
   rightArrow.click();
-}, 4000);
+}, 5000);
